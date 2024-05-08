df41d3dcc13d6a71b272afd4c7fcb9331d32d8c2
Yufan Sheng <syhily@gmail.com>
2024-05-09 01:47:13 +0800

    feat: design draft on the comments tables.

---
+++ models/migration/0000_dry_boom_boom.sql
@@ -0,0 +1,115 @@
+CREATE TABLE `duoshuo_comments` (
+	`id` serial AUTO_INCREMENT NOT NULL,
+	`create_time` timestamp NOT NULL DEFAULT (now()),
+	`update_time` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`deleted` bigint unsigned DEFAULT 0,
+	`user_id` bigint unsigned NOT NULL,
+	`post_id` bigint unsigned NOT NULL,
+	`content` longtext,
+	`pending` boolean DEFAULT false,
+	`collapsed` boolean DEFAULT false,
+	`pinned` boolean DEFAULT false,
+	`ip` varchar(16),
+	`ua` text,
+	`likes` bigint unsigned DEFAULT 0,
+	`parent_id` bigint unsigned,
+	`thread_id` bigint unsigned,
+	CONSTRAINT `duoshuo_comments_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE TABLE `duoshuo_comments_like` (
+	`id` serial AUTO_INCREMENT NOT NULL,
+	`create_time` timestamp NOT NULL DEFAULT (now()),
+	`update_time` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`comment_id` bigint unsigned NOT NULL,
+	`anonymous_token` char(128),
+	`user_id` bigint unsigned,
+	CONSTRAINT `duoshuo_comments_like_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE TABLE `duoshuo_configs` (
+	`id` serial AUTO_INCREMENT NOT NULL,
+	`create_time` timestamp NOT NULL DEFAULT (now()),
+	`update_time` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`name` varchar(128) NOT NULL,
+	`value` json NOT NULL,
+	CONSTRAINT `duoshuo_configs_id` PRIMARY KEY(`id`),
+	CONSTRAINT `duoshuo_configs_name_unique` UNIQUE(`name`)
+);
+--> statement-breakpoint
+CREATE TABLE `duoshuo_posts` (
+	`id` serial AUTO_INCREMENT NOT NULL,
+	`create_time` timestamp NOT NULL DEFAULT (now()),
+	`update_time` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`deleted` bigint unsigned DEFAULT 0,
+	`title` text,
+	`request_path` varchar(512) NOT NULL,
+	`clicks` bigint unsigned DEFAULT 0,
+	`likes` bigint unsigned DEFAULT 0,
+	CONSTRAINT `duoshuo_posts_id` PRIMARY KEY(`id`),
+	CONSTRAINT `duoshuo_posts_request_path_unique` UNIQUE(`request_path`)
+);
+--> statement-breakpoint
+CREATE TABLE `duoshuo_posts_identity` (
+	`id` serial AUTO_INCREMENT NOT NULL,
+	`create_time` timestamp NOT NULL DEFAULT (now()),
+	`update_time` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`deleted` bigint unsigned DEFAULT 0,
+	`identity` varchar(128) NOT NULL,
+	`post_id` bigint unsigned NOT NULL,
+	CONSTRAINT `duoshuo_posts_identity_id` PRIMARY KEY(`id`),
+	CONSTRAINT `duoshuo_posts_identity_identity_unique` UNIQUE(`identity`)
+);
+--> statement-breakpoint
+CREATE TABLE `duoshuo_posts_like` (
+	`id` serial AUTO_INCREMENT NOT NULL,
+	`create_time` timestamp NOT NULL DEFAULT (now()),
+	`update_time` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`post_id` bigint unsigned NOT NULL,
+	`anonymous_token` char(128),
+	`user_id` bigint unsigned,
+	CONSTRAINT `duoshuo_posts_like_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE TABLE `duoshuo_users` (
+	`id` serial AUTO_INCREMENT NOT NULL,
+	`create_time` timestamp NOT NULL DEFAULT (now()),
+	`update_time` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`deleted` bigint unsigned DEFAULT 0,
+	`email` varchar(256) NOT NULL,
+	`nick_name` varchar(64) NOT NULL,
+	`registered` boolean DEFAULT false,
+	`verified` boolean DEFAULT false,
+	`password` char(512),
+	`salt` varchar(128),
+	`admin` boolean DEFAULT false,
+	`friend` boolean DEFAULT false,
+	`homepage` varchar(256),
+	`last_login_ip` varchar(16),
+	`last_login_ua` text,
+	CONSTRAINT `duoshuo_users_id` PRIMARY KEY(`id`),
+	CONSTRAINT `duoshuo_users_email_unique` UNIQUE(`email`)
+);
+--> statement-breakpoint
+CREATE TABLE `duoshuo_users_mask` (
+	`id` serial AUTO_INCREMENT NOT NULL,
+	`create_time` timestamp NOT NULL DEFAULT (now()),
+	`update_time` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`nick_name` varchar(64) NOT NULL,
+	`user_id` bigint unsigned NOT NULL,
+	CONSTRAINT `duoshuo_users_mask_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE INDEX `comments_user_id_idx` ON `duoshuo_comments` (`user_id`);--> statement-breakpoint
+CREATE INDEX `comments_idx` ON `duoshuo_comments` (`deleted`,`post_id`,`thread_id`);--> statement-breakpoint
+CREATE INDEX `comments_parent_id_idx` ON `duoshuo_comments` (`parent_id`);--> statement-breakpoint
+CREATE INDEX `comments_like_comment_id_idx` ON `duoshuo_comments_like` (`comment_id`);--> statement-breakpoint
+CREATE INDEX `comments_like_user_id_id` ON `duoshuo_comments_like` (`user_id`);--> statement-breakpoint
+CREATE INDEX `posts_deleted_idx` ON `duoshuo_posts` (`deleted`);--> statement-breakpoint
+CREATE INDEX `posts_title_idx` ON `duoshuo_posts` (`title`);--> statement-breakpoint
+CREATE INDEX `posts_identity_post_id_idx` ON `duoshuo_posts_identity` (`post_id`);--> statement-breakpoint
+CREATE INDEX `posts_like_post_id_idx` ON `duoshuo_posts_like` (`post_id`);--> statement-breakpoint
+CREATE INDEX `posts_like_user_id_idx` ON `duoshuo_posts_like` (`user_id`);--> statement-breakpoint
+CREATE INDEX `users_deleted_idx` ON `duoshuo_users` (`deleted`);--> statement-breakpoint
+CREATE INDEX `users_mask_nick_name_idx` ON `duoshuo_users_mask` (`nick_name`);--> statement-breakpoint
+CREATE INDEX `users_mask_user_id_idx` ON `duoshuo_users_mask` (`user_id`);
--- models/schema.ts
+++ models/schema.ts
@@ -1,4 +1,16 @@
-import { bigint, mysqlTableCreator, serial, timestamp, varchar } from 'drizzle-orm/mysql-core';
+import {
+  bigint,
+  boolean,
+  char,
+  index,
+  json,
+  longtext,
+  mysqlTableCreator,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from 'drizzle-orm/mysql-core';
 
 import { defaultTablePrefix } from '@/utils/env';
 
@@ -8,23 +20,157 @@ import { defaultTablePrefix } from '@/utils/env';
  */
 const mysqlTable = mysqlTableCreator((name) => `${defaultTablePrefix}${name}`);
 
+const primaryKey: { mode: 'number'; unsigned: boolean } = { mode: 'number', unsigned: true };
+
 // Common columns for all the tables used in the duoshuo.
 const commonColumns = {
   id: serial('id').primaryKey(),
   createTime: timestamp('create_time').notNull().defaultNow(),
   updateTime: timestamp('update_time').notNull().defaultNow().onUpdateNow(),
-  // The 'deleted' column serves for logical deletion. A value of 0 indicates the record is active.
-  // When a record is deleted, the 'deleted' column will store the timestamp of the deletion.
-  deleted: bigint('deleted', { mode: 'number', unsigned: true }).default(0),
 };
 
-// The comments' table for storing the users' comments.
-export const comments = mysqlTable('comments', {
+const logicDeleteColumns = {
   ...commonColumns,
-});
+  // The 'deleted' column serves for logical deletion. A value of 0 indicates the record is active.
+  // When a record is deleted, the 'deleted' column will store the timestamp of the deletion.
+  deleted: bigint('deleted', primaryKey).default(0),
+};
 
+// The configuration table for storing all the system configurations.
+// This table only supports upsert.
+// Deletion isn't allowed on this table.
 export const configs = mysqlTable('configs', {
   ...commonColumns,
   name: varchar('name', { length: 128 }).notNull().unique(),
-  value: varchar('value', { length: 256 }).notNull(),
+  value: json('value').notNull(),
 });
+
+// The registered user and administrator.
+// If the user deleted his or her account, it can't be recovered because it's a logic deletion.
+export const users = mysqlTable(
+  'users',
+  {
+    ...logicDeleteColumns,
+    email: varchar('email', { length: 256 }).notNull().unique(),
+    nickName: varchar('nick_name', { length: 64 }).notNull(),
+    registered: boolean('registered').default(false),
+    verified: boolean('verified').default(false),
+    password: char('password', { length: 512 }),
+    salt: varchar('salt', { length: 128 }),
+    admin: boolean('admin').default(false),
+    friend: boolean('friend').default(false),
+    homepage: varchar('homepage', { length: 256 }),
+    lastLoginIP: varchar('last_login_ip', { length: 16 }),
+    lastLoginUA: text('last_login_ua'),
+  },
+  (table) => ({
+    deleteIdx: index('users_deleted_idx').on(table.deleted),
+  }),
+);
+
+// Some user may have more than one nickname in different time range.
+// But given they are all for the same user internally.
+// We would like to define it as a mask for the user.
+// Anyone can have the mast freely but with only one limitation.
+// That is, you can't have more than two masks which is the same as others.
+export const usersMask = mysqlTable(
+  'users_mask',
+  {
+    ...commonColumns,
+    nickname: varchar('nick_name', { length: 64 }).notNull(),
+    userId: bigint('user_id', primaryKey).notNull(),
+  },
+  (table) => ({
+    nicknameIdx: index('users_mask_nick_name_idx').on(table.nickname),
+    userIdIdx: index('users_mask_user_id_idx').on(table.userId),
+  }),
+);
+
+// Storing the posts' meta.
+export const posts = mysqlTable(
+  'posts',
+  {
+    ...logicDeleteColumns,
+    title: text('title'),
+    requestPath: varchar('request_path', { length: 512 }).notNull().unique(),
+    clicks: bigint('clicks', primaryKey).default(0),
+    likes: bigint('likes', primaryKey).default(0),
+  },
+  (table) => ({
+    deletedIdx: index('posts_deleted_idx').on(table.deleted),
+    titleIdx: index('posts_title_idx').on(table.title),
+  }),
+);
+
+// The mapping for the post's identity and the post's id.
+// We can add multiple identities to the same post id.
+// This is quite useful for migration and when the user changes the post's identity.
+export const postsIdentity = mysqlTable(
+  'posts_identity',
+  {
+    ...logicDeleteColumns,
+    identity: varchar('identity', { length: 128 }).notNull().unique(),
+    postId: bigint('post_id', primaryKey).notNull(),
+  },
+  (table) => ({
+    postIdIdx: index('posts_identity_post_id_idx').on(table.postId),
+  }),
+);
+
+// The table for storing a user-unrelated token with the post id.
+// Used for marking if someone has liked this posts anonymously.
+// We only store the anonymous like within a month by default.
+// The likes from login users will be kept forever.
+// The administrator can configure the storing time range for anonymous like.
+export const postsLike = mysqlTable(
+  'posts_like',
+  {
+    ...commonColumns,
+    postId: bigint('post_id', primaryKey).notNull(),
+    anonymousToken: char('anonymous_token', { length: 128 }),
+    userId: bigint('user_id', primaryKey),
+  },
+  (table) => ({
+    postIdIdx: index('posts_like_post_id_idx').on(table.postId),
+    userIdIdx: index('posts_like_user_id_idx').on(table.userId),
+  }),
+);
+
+// The comments' table for storing the users' comments.
+export const comments = mysqlTable(
+  'comments',
+  {
+    ...logicDeleteColumns,
+    userId: bigint('user_id', primaryKey).notNull(),
+    postId: bigint('post_id', primaryKey).notNull(),
+    content: longtext('content'),
+    pending: boolean('pending').default(false),
+    collapsed: boolean('collapsed').default(false),
+    pinned: boolean('pinned').default(false),
+    ip: varchar('ip', { length: 16 }),
+    ua: text('ua'),
+    likes: bigint('likes', primaryKey).default(0),
+    parentId: bigint('parent_id', primaryKey),
+    threadId: bigint('thread_id', primaryKey),
+  },
+  (table) => ({
+    userIdIdx: index('comments_user_id_idx').on(table.userId),
+    commentsIdx: index('comments_idx').on(table.deleted, table.postId, table.threadId),
+    parentIdIdx: index('comments_parent_id_idx').on(table.parentId),
+  }),
+);
+
+// The optional like button for comments.
+export const commentsLike = mysqlTable(
+  'comments_like',
+  {
+    ...commonColumns,
+    commentId: bigint('comment_id', primaryKey).notNull(),
+    anonymousToken: char('anonymous_token', { length: 128 }),
+    userId: bigint('user_id', primaryKey),
+  },
+  (table) => ({
+    commentIdIdx: index('comments_like_comment_id_idx').on(table.commentId),
+    userIdIdx: index('comments_like_user_id_id').on(table.userId),
+  }),
+);
